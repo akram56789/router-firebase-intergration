d56a356bdcfa6a9b3fec7a92cdc37a61a6da6e5e
akram56789 <akramhossain56789@gmail>
2022-04-12 03:10:30 +0600

    heder-done-router-i-done

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4NzKntZ6L9p-vzH6bRs76JmMNL6YWZbM",
+  authDomain: "router-firebase-integrat-e421c.firebaseapp.com",
+  projectId: "router-firebase-integrat-e421c",
+  storageBucket: "router-firebase-integrat-e421c.appspot.com",
+  messagingSenderId: "946474795345",
+  appId: "1:946474795345:web:1469bac0998ae5d24b1370"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
